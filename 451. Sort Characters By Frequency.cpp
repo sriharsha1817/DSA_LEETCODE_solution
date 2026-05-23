ec8bf0d6537712705879b8e0b5835ef24a984667
Sriharsha <sriharsha8171@gmail.com>
2026-05-23 12:08:55 +0530

    Add 451. Sort Characters By Frequency solution

---
+++ 451. Sort Characters By Frequency.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int> freq;
+        for(char c:s)
+        {
+            freq[c]++;
+        }
+        sort(s.begin(), s.end(),
+        [&](char a,char b)
+        {
+            if(freq[a]==freq[b])
+                return a<b;
+
+            return freq[a]>freq[b];
+        });
+        return s;
+    }
+};
